0a8823cf574a1029c3809d150335ed623f38acea
Castro <103647024+joaoguicastro@users.noreply.github.com>
2024-07-03 21:01:29 -0300

    utilizando cookies no fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,30 @@ import { randomUUID } from "node:crypto"
 // Request Body
 
 export async function transactionsRoutes(app: FastifyInstance){
+    app.get('/summary', async () => {
+        const summary = await knex('transactions').sum('amount', { as: 'amount' })
+
+        return {summary}
+    })
+
+    app.get('/', async (req, res) => {
+        const transactions = await knex('transactions').select()
+
+        return { transactions }
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionsParamsSchame = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = getTransactionsParamsSchame.parse(request.params)
+
+        const transaction = await knex('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+
     app.post('/', async (request, reply) => {
         // { title, amount, type: credit or debit }
 
